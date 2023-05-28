3839ec2f8e87590eb6db78626aaf9030252bad00
George Liu <zhihongliu491@gmail.com>
2023-05-28 22:31:21 +0800

    增加定时运行save.py的功能

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-import csv
-
 from flask import Flask, render_template
-import sqlite3
+import csv
+from apscheduler.schedulers.background import BackgroundScheduler
+import save
+
+
+scheduler = BackgroundScheduler()
+
+
+def job():
+    """
+    定时任务，每次执行调用save.py进行数据爬取以及更新存储
+    :return:
+    """
+    path = './data/data.csv'
+    save.csv_save(path)
 
 
 app = Flask(__name__)
+
+scheduler.add_job(job(), id="save", trigger='interval', hours=6)
 
 
 @app.route('/')
 def index():  # put application's code here
     csvfile = open('./data/test.csv', newline='')
-    infos = csv.DictReader(csvfile)
-
+    infos = csv.DictReader(csvfile, delimiter=',')
     return render_template('index.html', infos=infos)
 
 
 if __name__ == '__main__':
+    scheduler.start()
     app.run()
