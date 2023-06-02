fb023c7b9fae7f55c67cc36de988faf4e1d97423
George Liu <zhihongliu491@gmail.com>
2023-06-02 16:40:24 +0800

    remove sheduler job

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template
 import csv
-import save
 from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from waitress import serve
+# from apscheduler.schedulers.background import BackgroundScheduler
+# from waitress import serve
 
 
 app = Flask(__name__)
 
 
-def job():
-    """
-    定时任务，每次执行调用save.py进行数据爬取以及更新存储
-    :return:
-    """
-    path = './data/data.csv'
-    save.csv_save(path)
-    print(f"job {datetime.now()} ok")
-
-
-scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(job, 'interval', hours=6)
-scheduler.start()
-# set up the schedule job
+# def job():
+#     """
+#     定时任务，每次执行调用save.py进行数据爬取以及更新存储
+#     :return:
+#     """
+#     path = './data/data.csv'
+#     save.csv_save(path)
+#     print(f"job {datetime.now()} ok")
+#
+#
+# scheduler = BackgroundScheduler(daemon=True)
+# scheduler.add_job(job, 'interval', hours=6)
+# scheduler.start()
+# # set up the schedule job
 
 
 @app.route('/')
@@ -33,6 +32,6 @@ def index():  # put application's code here
 
 
 if __name__ == '__main__':
-    # app.run(
-    serve(app, host='0.0.0.0', port=5000)
+    # serve(app, host='0.0.0.0', port=5000)
+    app.run()
 
